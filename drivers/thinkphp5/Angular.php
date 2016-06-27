e64d1299099e2cb94142a423e115c9f417822f1f
翟帅干 <zhaishuaigan@qq.com>
2016-06-28 00:49:07 +0800

    更新驱动 支持多级控制器的模版显示。 兼容display用法.

--- drivers/thinkphp5/Angular.php
+++ drivers/thinkphp5/Angular.php
@@ -14,42 +14,79 @@ use think\Config;
 use think\Request;
 use think\App;
 use think\angular\Angular as AngularTpl;
+use think\template\driver\File as Storage;
 
-class Angular {
+class Angular
+{
 
     private $template = null;
     private $config   = [];
     private $storage  = null;
 
-    public function __construct($config = []) {
+    public function __construct($config = [])
+    {
         $this->config   = [
             'tpl_path'         => App::$modulePath . 'view' . DS,
-            'tpl_suffix'       => Config::get('view.engine_config.suffix') ? : '.html',
-            'tpl_cache_path'   => CACHE_PATH,
-            'tpl_cache_suffix' => Config::get('view.engine_config.cache') ? : '.php',
+            'tpl_suffix'       => '.' . (Config::get('template.view_suffix') ? : 'html'),
+            'tpl_cache_path'   => RUNTIME_PATH . 'temp' . DS,
+            'tpl_cache_suffix' => Config::get('template.cache_view_suffix') ? : '.php',
             'attr'             => 'php-',
         ];
+        dump($this->config);
         $this->template = new AngularTpl($this->config);
         // 初始化模板编译存储器
-        $storage       = Config::get('compile_type') ? : '\\think\\template\\driver\\File';
-        $this->storage = new $storage();
+        $this->storage  = new Storage();
     }
 
-    public function fetch($template, $data = [], $cache = []) {
-        if (!$template) {
-            $request    = Request::instance();
-            $controller = $request->controller();
-            $action = $request->action();
-            $template       = $controller. DS . $action;
-        }
+    /**
+     * 获取模版运行结果
+     * @param string $template 模版地址
+     * @param array $data 模版数据
+     * @param array $config 配置
+     * @return string
+     */
+    public function fetch($template, $data = [], $config = [])
+    {
+        // 处理模版地址
+        $template = $this->parseTemplatePath($template);
+
         // 根据模版文件名定位缓存文件
-        $tpl_cache_file = CACHE_PATH . 'angular_' . md5($template) . '.php';
+        $tpl_cache_file = $this->config['tpl_cache_path'] . 'angular_' . md5($template) . '.php';
         if (App::$debug || !is_file($tpl_cache_file) || !$this->storage->check($tpl_cache_file, 0)) {
             // 编译模板内容
             $content = $this->template->compiler($template, $data);
             $this->storage->write($tpl_cache_file, $content);
         }
         $this->storage->read($tpl_cache_file, $data);
+    }
+
+    /**
+     * fetch的别名
+     * @param string $template 模版地址
+     * @param array $data 模版数据
+     * @param array $config 配置
+     * @return string
+     */
+    public function display($template, $data = [], $config = [])
+    {
+        return $this->fetch($template, $data, $config);
+    }
+
+    /**
+     * 如果模版为空, 则通过URL参数获取模版地址
+     * @param string $template 模版地址
+     * @return string
+     */
+    public function parseTemplatePath($template)
+    {
+        if (!$template) {
+            $request    = Request::instance();
+            $controller = $request->controller();
+            $action     = $request->action();
+            $template   = $controller . DS . $action;
+            $template   = str_replace('.', DS, $template);
+        }
+        return $template;
     }
 
 }
